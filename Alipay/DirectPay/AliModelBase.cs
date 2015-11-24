1e3ee682c03d8a6d54f34046482a47b2968aab77
wangyang <wang.yang@etaostars.com>
2015-11-24 18:14:34 +0800

    dsf

--- Alipay/DirectPay/AliModelBase.cs
+++ Alipay/DirectPay/AliModelBase.cs
@@ -12,10 +12,11 @@ namespace EasyOa.Common.Alipay.DirectPay
     {
         [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "Required")]
         public string service { get; set; }   //接口名称
-        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "Required")]
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "Required")]
         public string partner { get; set; }   //合作方id
         public string _input_charset { get { return "utf-8"; } }   //字符集
         public string sign_type { get { return "MD5"; } }   //签名类型
+
         public string sign { get; set; }
         public string notify_url { get; set; }  //可空  //服务器异步通知
         public string return_url { get; set; }  //可空 页面调转同步通知
